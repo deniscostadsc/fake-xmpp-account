99dc0f1887580cf8b272f94f82c03104d55a2b3d
Denis Costa <deniscostadsc@gmail.com>
2018-08-21 12:46:14 -0300

    Change script

--- run_pidgins.sh
+++ run_pidgins.sh
@@ -4,6 +4,6 @@ export PIDGIN_MULTI_INST=1
 
 git checkout .
 
-nohup pidgin -mc  ./fakeaccount1 > fakeaccount1.out 2>&1 &
-nohup pidgin -mc  ./fakeaccount2 > fakeaccount2.out 2>&1 &
+nohup pidgin -dmc ./fakeaccount1 > fakeaccount1.out 2>&1 &
+#nohup pidgin -dmc ./fakeaccount2 > fakeaccount2.out 2>&1 &
 
