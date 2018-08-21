8e082e1e4da1c079f45814d8240779277c4ec8b0
Denis Costa <deniscostadsc@gmail.com>
2018-08-21 13:25:59 -0300

    bob and alice for the win

--- run_pidgins.sh
+++ run_pidgins.sh
@@ -2,8 +2,6 @@
 
 export PIDGIN_MULTI_INST=1
 
-git checkout .
-
-nohup pidgin -dmc ./fakeaccount1 > fakeaccount1.out 2>&1 &
-#nohup pidgin -dmc ./fakeaccount2 > fakeaccount2.out 2>&1 &
+nohup pidgin -dmc ./alice > alice.log 2>&1 &
+nohup pidgin -dmc ./bob > bob.log 2>&1 &
 
