4a5ae967fea9f284d5c7e6b13f9159960bdbe4df
Denis Costa <deniscostadsc@gmail.com>
2018-08-21 18:34:38 -0300

    Fix script to run pidgin

--- run_pidgins.sh
+++ run_pidgins.sh
@@ -4,6 +4,6 @@ export PIDGIN_MULTI_INST=1
 
 now=$(date +"%Y-%m-%d-%H-%M-%S")
 
-nohup pidgin -dmc ./alice > "$now-alice.log" 2>&1 &
-nohup pidgin -dmc ./bob > "$now-bob.log" 2>&1 &
+nohup pidgin -dmc ./alice > logs/"$now-alice.log" 2>&1 &
+nohup pidgin -dmc ./bob > logs/"$now-bob.log" 2>&1 &
 
