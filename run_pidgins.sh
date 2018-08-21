e14d3aa13b69e3b6d01084ee1548346cec2e60b4
Denis Costa <deniscostadsc@gmail.com>
2018-08-21 15:56:37 -0300

    Add more things

--- run_pidgins.sh
+++ run_pidgins.sh
@@ -2,6 +2,8 @@
 
 export PIDGIN_MULTI_INST=1
 
-nohup pidgin -dmc ./alice > alice.log 2>&1 &
-nohup pidgin -dmc ./bob > bob.log 2>&1 &
+now=$(date +"%Y-%m-%d-%H-%M-%S")
+
+nohup pidgin -dmc ./alice > "$now-alice.log" 2>&1 &
+nohup pidgin -dmc ./bob > "$now-bob.log" 2>&1 &
 
